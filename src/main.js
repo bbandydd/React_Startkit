2f0b1740eb573eaf97bc76c0df104b9e78315e6e
Andy Tsai <andy@Andy-Tsai.local>
2016-12-17 11:50:06 +0800

    Completed PartI

--- src/main.js
+++ src/main.js
@@ -1,10 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Hello extends React.Component {
-  render(){
-    return (<div>Hel123ljoijoijojo</div>);
-  }
+class Panel extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            number: 0
+        }
+    }
+
+    increment = () => {
+        let newState = {
+            number: this.state.number + 1
+        }
+
+        this.setState(newState);
+    }
+
+    render() {
+      return (
+        <div>
+          <Show num={this.state.number} />
+          <Btn increment={this.increment} />
+        </div>
+      )
+    }
 }
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+class Show extends React.Component {
+    render() {
+      const { num } = this.props;
+
+      return (
+        <div>
+          { num }
+        </div>
+      )
+    }
+}
+
+class Btn extends React.Component {
+    render() {
+      const { increment } = this.props;
+
+      return (
+        <div>
+          <button onClick={increment}> + </button>
+        </div>
+      )
+    }
+} 
+
+ReactDOM.render(
+  <Panel />
+  , document.getElementById('app'));
