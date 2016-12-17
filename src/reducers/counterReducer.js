d6fe0220cd3154606268bc2852721d2df6f877e3
Andy Tsai <andy@Andy-Tsai.local>
2016-12-17 14:15:11 +0800

    add counterReducer

---
+++ src/reducers/counterReducer.js
@@ -0,0 +1,10 @@
+const counterReducer = (state = 0, action) => {
+    switch(action.type) {
+        case 'INCREMENT':
+            return state + 1;
+        default: 
+            return state;
+    }
+}
+
+export default counterReducer;
