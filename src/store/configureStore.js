df5dd884cf3b05200f6de57f2714144164c0ffd6
Andy Tsai <andy@Andy-Tsai.local>
2016-12-17 14:28:19 +0800

    add configureStore

---
+++ src/store/configureStore.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers } from 'redux';
+
+import counterReducer from '../reducers/counterReducer';
+
+const rootReducer = combineReducers({
+    counterReducer
+})
+
+export default createStore(rootReducer);
