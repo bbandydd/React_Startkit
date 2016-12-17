305f31c38689b8acdc25e0fc1d7111a3a8b818e1
Andy Tsai <andy@Andy-Tsai.local>
2016-12-17 14:19:59 +0800

    action add number

--- src/actions/counterAction.js
+++ src/actions/counterAction.js
@@ -1,5 +1,6 @@
 export const incrementAction = () => {
     return {
-        type: 'INCREMENT'
+        type: 'INCREMENT',
+        number: 1
     }
 }
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,7 +1,7 @@
 const counterReducer = (state = 0, action) => {
     switch(action.type) {
         case 'INCREMENT':
-            return state + 1;
+            return state + action.number;
         default: 
             return state;
     }
